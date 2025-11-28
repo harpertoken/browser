f8c7344816f991541ed4df3bb64e6e34fcfc08a7
Niladri Das <bniladridas@users.noreply.github.com>
2025-11-28 20:21:53 +0530

    feat: add basic browser functionality

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Browser',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const BrowserPage(),
+    );
+  }
+}
+
+class BrowserPage extends StatefulWidget {
+  const BrowserPage({super.key});
+
+  @override
+  State<BrowserPage> createState() => _BrowserPageState();
+}
+
+class _BrowserPageState extends State<BrowserPage> {
+  final TextEditingController urlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    urlController.text = 'https://www.google.com';
+  }
+
+  void _loadUrl(String url) async {
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      url = 'https://$url';
+    }
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      // Handle error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Could not launch $url')),
+      );
+    }
+    urlController.text = url;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: urlController,
+          decoration: const InputDecoration(
+            hintText: 'Enter URL',
+            border: InputBorder.none,
+          ),
+          onSubmitted: _loadUrl,
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Enter a URL in the bar above and press Enter to open it in your default browser.',
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
