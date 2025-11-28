2d129af6f25e0d07b745ff6e7059f317e742c849
Niladri Das <bniladridas@users.noreply.github.com>
2025-11-29 00:08:30 +0530

    fix: scope warning suppression

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright 2025 bniladridas. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 import 'package:window_manager/window_manager.dart';
